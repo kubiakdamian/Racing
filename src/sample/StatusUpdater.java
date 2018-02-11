ca8036882d685ccf2b6f4067a4e034a3d1ddb5c9
kubiakdamian <kubiak.damian96@gmail.com>
2018-02-11 18:32:24 +0100

    Gotowe zapisywanie oraz sczytywanie wyników z bazy danych

--- src/sample/StatusUpdater.java
+++ src/sample/StatusUpdater.java
@@ -9,16 +9,17 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
-
 public class StatusUpdater extends Pane{
     Text status = new Text();
     Label label = new Label("Enter name:");
     TextField textField = new TextField ();
     Button submitBtn = new Button("Submit");
     private RecordDAO recordDAO;
+    private OptionDialog optionDialog;
 
     public StatusUpdater(double x, double y){
         recordDAO = new RecordDAO();
+        optionDialog = new OptionDialog();
         setTranslateX(x);
         setTranslateY(y - 50);
         setStyle("-fx-background-color: #CCC;" +
@@ -39,7 +40,15 @@ public class StatusUpdater extends Pane{
         submitBtn.setStyle("-fx-background-color: aquamarine;" + "-fx-cursor: hand");
         submitBtn.setDisable(true);
         submitBtn.setMinSize(100, 30);
-        submitBtn.setOnAction((event -> recordDAO.save(textField.getText(), (double) Game.getCurrentTime() / 1000)));
+        submitBtn.setOnAction(event -> {
+            recordDAO.save(textField.getText(), (double) Game.getCurrentTime() / 1000);
+            try {
+                optionDialog.start(OptionDialog.window);
+                OptionDialog.window.centerOnScreen();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
         textField.textProperty().addListener((ov, oldLength, newLength) -> {
             if(newLength.length() < 4 || newLength.length() > 15){
